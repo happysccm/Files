fb19b0d1e976c6d54540cc01ddf76363caccc2eb
HappySCCM <hello@happysccm.com>
2017-01-05 14:56:57 +1100

    HP Ownership Tag
    Added new script to check the Ownership Tag instead of Asset Tag if machine = HP

---
+++ Check for Asset Tag - OSD AssetTag Check - Most code by Nickolaj and Dave Green/AssetTag - HP Ownership.ps1
@@ -0,0 +1,89 @@
+﻿Function Load-Form 
+{
+    $Form.Controls.Add($GBLabel)
+    $Form.Controls.Add($ButtonOK)
+    $Form.Add_Shown({$Form.Activate()})
+    [void] $Form.ShowDialog()
+}
+ 
+
+
+
+
+$model = Get-WmiObject -Class Win32_ComputerSystem | ForEach-Object {$_.Model}
+    If ($model -like "*vmware*")  {
+    Write-Host "vm"
+    exit 0
+    }
+
+    ElseIf ($model -like "*Parallels*")  {
+    Write-Host "Parallels"
+    exit 0
+    } 
+
+    ElseIf ($model -like "*Virtual Machine*")  {
+    Write-Host "Virtual Machine"
+    exit 0
+    } 
+
+    ElseIf ($model -like "*Mac*")  {
+    Write-Host "Mac"
+    exit 0
+    } 
+
+
+$tsenv = New-Object -COMObject Microsoft.SMS.TSEnvironment
+$machineName =  $tsenv.Value("_SMSTSMachineName")
+
+If ([string]::IsNullOrWhitespace($machineName) -or ($machineName -like 'MININT*')) {
+
+$manufacturer  = Get-WmiObject -Class Win32_ComputerSystem | ForEach-Object {$_.Manufacturer}
+
+
+If (($manufacturer -like '*Hewlett-Packard*') -or ($manufacturer -like '*HP*')) {
+$bios = Get-WmiObject -Name root\hp\instrumentedbios -Cl HP_BiosSetting | ? {$_.Name -like "*Ownership Tag*"} | Select -ExpandProperty Value
+}
+Else {
+$bios = Get-WmiObject -Class Win32_SystemEnclosure | ForEach-Object {$_.SMBIOSAssetTag}
+}
+
+If ([string]::IsNullOrWhitespace($bios)) {
+
+[void][System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+[void][System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+$Global:ErrorProvider = New-Object System.Windows.Forms.ErrorProvider
+
+$Form = New-Object System.Windows.Forms.Form    
+$Form.Size = New-Object System.Drawing.Size(285,140)  
+$Form.MinimumSize = New-Object System.Drawing.Size(285,140)
+$Form.MaximumSize = New-Object System.Drawing.Size(285,140)
+$Form.StartPosition = "CenterScreen"
+$Form.SizeGripStyle = "Hide"
+$Form.Text = "Asset Tag not found"
+$Form.ControlBox = $false
+$Form.TopMost = $true
+
+$GBLabel = New-Object System.Windows.Forms.Label
+$GBLabel.Location = New-Object System.Drawing.Size(20,10)
+$GBLabel.Size = New-Object System.Drawing.Size(225,50)
+$GBLabel.Text = "Please configure the Dell Asset Tag or HP Ownership Tag in the bios and try again"
+
+$ButtonOK = New-Object System.Windows.Forms.Button
+$ButtonOK.Location = New-Object System.Drawing.Size(195,70)
+$ButtonOK.Size = New-Object System.Drawing.Size(50,20)
+$ButtonOK.Text = "OK"
+$ButtonOK.TabIndex = "2"
+$ButtonOK.Add_Click({[System.Environment]::Exit(1)})
+
+$Form.KeyPreview = $True
+$Form.Add_KeyDown({if ($_.KeyCode -eq "Enter"){[System.Environment]::Exit(1)}})
+
+Load-Form
+
+}
+
+$TSEnv.Value("OSDComputerName") = $bios
+
+
+}
